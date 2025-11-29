3fe1c4e92f96abb2b0e2fbae3609720a3c243368
Kangmoongu <sprtms5325@naver.com>
2025-11-29 22:18:42 +0900

    feat: Holiday, Country Repository 작성

---
+++ src/main/java/com/holidaykeeper/repository/CountryRepository.java
@@ -0,0 +1,10 @@
+package com.holidaykeeper.repository;
+
+import com.holidaykeeper.entity.Country;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, UUID> {
+    
+}
